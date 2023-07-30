f306c2e1adc83394bb4b6955926cf8868c09d738
Silva9507 <luisk-95@hotmail.com>
2023-07-30 17:26:52 -0500

    intento mil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from '../app/app.component';
+import { NewsComponent } from './components/news/news.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { FiltersButtonsComponent } from './components/filters-buttons/filters-buttons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsComponent,
+    HeaderComponent,
+    SideBarComponent,
+    FiltersButtonsComponent
   ],
   imports: [
     BrowserModule
